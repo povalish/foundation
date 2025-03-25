94dd7e4e39b35d4818064975b609e84157802529
Nikita Povalishev <np@skl.vc>
2025-03-25 21:27:57 +0300

    Bubble sorting

--- src/algorithms/sorting/bubble.ts
+++ src/algorithms/sorting/bubble.ts
@@ -10,10 +10,12 @@
 
 export function bubbleSort(array: number[]): number[] {
   let lastSortedIndex = array.length;
+  let operations = 0;
 
   for (let i = 0; i <= lastSortedIndex; i++) {
     for (let j = 0; j < lastSortedIndex - 1; j++) {
       if (array[j] > array[j + 1]) {
+        operations += 1;
         const tmp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = tmp;
@@ -22,6 +24,36 @@ export function bubbleSort(array: number[]): number[] {
 
     lastSortedIndex -= 1;
   }
+
+  console.log(`Operations - ${operations}`);
+
+  return array;
+}
+
+export function bubbleSortV2(array: number[]): number[] {
+  let lastSortedIndex = array.length - 1;
+  let isArraySorted = false;
+  let operations = 0;
+
+  while (!isArraySorted) {
+    isArraySorted = true;
+
+    for (let i = 0; i <= lastSortedIndex; i++) {
+      if (array[i] > array[i + 1]) {
+        isArraySorted = false;
+        operations += 1;
+
+        // swap
+        const tmp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = tmp;
+      }
+    }
+
+    lastSortedIndex -= 1;
+  }
+
+  console.log(`Operations - ${operations}`);
 
   return array;
 }
--- src/algorithms/sorting/tests/bubble.test.ts
+++ src/algorithms/sorting/tests/bubble.test.ts
@@ -1,11 +1,19 @@
-import { bubbleSort } from '../bubble';
+import { bubbleSort, bubbleSortV2 } from '../bubble';
 
 describe('binary search', () => {
-  const unSortedArray = [8, 3, 5, 2, 6, 1, 7, 4];
-  const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8];
+  test('should sort array with v1', () => {
+    const unSortedArray = [8, 3, 5, 2, 6, 1, 7, 4];
+    const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  test('should sort array', () => {
     const result = bubbleSort(unSortedArray);
+    expect(result).toEqual(sortedArray);
+  });
+
+  test('should sort array with v2', () => {
+    const unSortedArray = [8, 3, 5, 2, 6, 1, 7, 4];
+    const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    const result = bubbleSortV2(unSortedArray);
     expect(result).toEqual(sortedArray);
   });
 });
