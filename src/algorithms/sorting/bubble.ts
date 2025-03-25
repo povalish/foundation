6e1517a91bd2e4a842058c04907af731ea699172
Nikita Povalishev <np@skl.vc>
2025-03-25 21:08:03 +0300

    Bubble sorting

--- src/algorithms/sorting/bubble.ts
+++ src/algorithms/sorting/bubble.ts
@@ -1,3 +1,27 @@
+/**
+ * Bubble Sort.
+ * Complexity: O(N^2)
+ *
+ * Description
+ * The task is pushing the largest element to the end of the list.
+ * - compare the elements with each other and swap them.
+ * - along the way, saving the index of the last sorted part.
+ */
+
 export function bubbleSort(array: number[]): number[] {
+  let lastSortedIndex = array.length;
+
+  for (let i = 0; i <= lastSortedIndex; i++) {
+    for (let j = 0; j < lastSortedIndex - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+
+    lastSortedIndex -= 1;
+  }
+
   return array;
 }
--- src/algorithms/sorting/tests/bubble.test.ts
+++ src/algorithms/sorting/tests/bubble.test.ts
@@ -1,8 +1,8 @@
 import { bubbleSort } from '../bubble';
 
-describe.skip('binary search', () => {
-  const unSortedArray = [4, 2, 7, 1, 3];
-  const sortedArray = [1, 2, 3, 4, 7];
+describe('binary search', () => {
+  const unSortedArray = [8, 3, 5, 2, 6, 1, 7, 4];
+  const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
   test('should sort array', () => {
     const result = bubbleSort(unSortedArray);
